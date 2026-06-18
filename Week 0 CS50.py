45368ba2c9780997b482ff6fc91d29825cc8cd61
Oscar0145 <oscar.ferguson2018@icloud.com>
2026-06-18 20:40:45 +0100

    Add files via upload

---
+++ Week 0 CS50.py
@@ -0,0 +1,21 @@
+#Week 0
+
+#The print() function displays output to the terminal.
+print("hello")
+
+#Variables store information that can be used later in the program.
+name = "Alice"
+age = 21
+print(name)
+print(age)
+
+#The input() function allows the user to enter information.
+name = input("What is your name? ")
+print(f"Hello, {name}!")
+
+#To perform calculations, user input must often be converted into a numerical data type.
+age = int(input("Enter your age: "))
+
+#f-Strings provide a clean way to insert variables into text.
+name = "Alice"
+print(f"Hello, {name}!")
