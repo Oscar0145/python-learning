5b073c4ca09823891ac36d1e2b12515f0c306d43
Oscar0145 <oscar.ferguson2018@icloud.com>
2026-06-25 18:10:32 +0100

    Add files via upload

---
+++ week 8 CS50.py
@@ -0,0 +1,125 @@
+#Week 8
+#Object-oriented programming (OOP) is a compelling solution to programming-related problems.
+name = input("Name: ")
+house = input("House: ")
+print(f"{name} from {house}")
+
+def main():
+    name = get_name()
+    house = get_house()
+    print(f"{name} from {house}")
+
+
+def get_name():
+    return input("Name: ")
+
+
+def get_house():
+    return input("House: ")
+
+
+if __name__ == "__main__":
+    main()
+
+#We can further simplify our program by storing the student as a tuple
+def main():
+    name, house = get_student()
+    print(f"{name} from {house}")
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return name, house
+
+
+if __name__ == "__main__":
+    main()
+
+#Classes are a way by which, in object-oriented programming,
+#we can create our own type of data and give them names.
+
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
+
+
+if __name__ == "__main__":
+    main()
+
+#What if something goes wrong?
+
+lass Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
+
+
+if __name__ == "__main__":
+    main()
+#we define properties using function “decorators”,
+#which begin with @. Modify your code as follows:
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Invalid name")
+        self.name = name
+        self.house = house
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+    # Getter for house
+    @property
+    def house(self):
+        return self._house
+
+    # Setter for house
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
+
+
+def main():
+    student = get_student()
+    print(student)
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
+
+
+if __name__ == "__main__":
+    main()
+
